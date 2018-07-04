26b424ad9d34a4ad267cd6c06b01edae85939a98
Savona Alessandro <36107882+PageFaultHandler@users.noreply.github.com>
2018-07-04 12:40:28 +0200

    Fixed some conflicts between tables

--- Tables.sql
+++ Tables.sql
@@ -1,15 +1,15 @@
-set search_path to  'progettoBD';
+set search_path to  'ProgettoBDfinale';
 
 
 CREATE TABLE Utente(
-username varchar(20) primary key,
+username varchar(22) primary key,
 psw varchar(20) not null,
 nome varchar(20) not null,
 cognome varchar(20) not null,
 annoN date not null,
-luogoN varchar(20) not null,
+luogoN varchar(35) not null,
 foto boolean,
-tel integer(10) not null,
+tel numeric(10) not null,
 matricola varchar(8) not null,
 CorsoDiStudio varchar(20) not null,
 tipo varchar(8) not null
@@ -18,7 +18,7 @@ check (annoN < current_date)
 );
 
 CREATE TABLE Torneo(
-id varchar(20) primary key,
+id numeric(8) primary key,
 restrizioni varchar(50) default null,
 descrizione varchar(60) default null,
 modalità varchar(20) not null,
@@ -34,19 +34,19 @@ check(tipo = 'singolo' or tipo = 'a squadre')
 CREATE TABLE Categoria(
 nome varchar(20) primary key,
 regolamento varchar(50) not null,
-numGiocatori int not null,
+numGiocatori numeric(2) not null,
 foto boolean default false
 );
 
 CREATE TABLE Squadra(
+id numeric(8) primary key,
 nome varchar(20) not null,
 categoria varchar(20) not null,
 creatore varchar(20) default null,
-maxGiocatori integer(2) not null,
-minGiocatori integer(2) not null,
+maxGiocatori numeric(2) not null,
+minGiocatori numeric(2) not null,
 coloreMaglia varchar(10) not null,
 note varchar(50) default null,
-primary key(nome,categoria),
 foreign key(categoria) references Categoria(nome) on delete  no action on update cascade,
 foreign key(creatore) references Utente(username) on delete  no action on update cascade,
 check(minGiocatori < maxGiocatori)
@@ -57,7 +57,7 @@ check(minGiocatori < maxGiocatori)
 CREATE TABLE Impianto (
 nome varchar(20) primary key,
 via varchar(50) not null,
-telefono integer(10) not null,
+telefono numeric(10) not null,
 email varchar(20) not null,
 longitudine float not null,
 latitudine float not null
@@ -65,10 +65,10 @@ latitudine float not null
 
 
 CREATE TABLE Evento (
-id varchar(20) primary key,
+id numeric(8) primary key,
 data date not null,
 stato varchar(15) not null,
-durata integer(3) not null,
+durata numeric(3) not null,
 nomeSquadraCasa varchar(20) default null,
 nomeSquadraOspite varchar(20) default null,
 impianto varchar(20) not null,
@@ -86,38 +86,39 @@ check(tipo = 'singolo' or tipo = 'a squadre')
 
 
 
-﻿CREATE TABLE Forma(
-idT integer(8) references Torneo (id) on delete  no action on update cascade,
-idEv integer(8) references Evento (id) on delete  no action on update cascade,
+CREATE TABLE Forma(
+idT numeric(8) references Torneo (id) on delete  no action on update cascade,
+idEv numeric(8) references Evento (id) on delete  no action on update cascade,
 primary key(idT, idEv)
 );
 
 CREATE TABLE PartecipaEv(
-nomeS varchar(30) references Squadra (nomeS)  on delete  no action on update cascade,
-nomeC varchar(30) references Categoria (nomeC)on delete  no action on update cascade,
-idEv integer(8) references Evento (id) on delete  no action on update cascade,
-punti integer(5) not null,
-primary key(nomeS, nomeC, idEv)
+idSquadra numeric(8) references Squadra(id)  on delete  no action on update cascade,
+nomeC varchar(30) references Categoria(nome) on delete  no action on update cascade,
+idEv numeric(8) references Evento (id) on delete  no action on update cascade,
+punti numeric(5) not null,
+primary key(idSquadra, nomeC, idEv)
 );
 
 CREATE TABLE UtenteGioca(
 username varchar(30) references Utente (username) on delete  no action on update cascade,
-idEv integer(8) references Evento (id) on delete  no action on update cascade,
-setvinti integer(3),
+idEv numeric(8) references Evento (id) on delete  no action on update cascade,
+setvinti numeric(3),
 primary key(username, idEv)
 );
 
 CREATE TABLE Gare(
 username varchar(30) references Utente (username)on delete  no action on update cascade,
-nomeC varchar(30) references Categoria (nomeC) on delete  no action on update cascade,
-gareDisputate integer(5),
+nomeC varchar(30) references Categoria (nome) on delete  no action on update cascade,
+gareDisputate numeric(5),
 primary key(username, nomeC)
 );
 
 CREATE TABLE Valutazione(
 usernameValutatore varchar(30) references Utente (username) on delete  no action on update cascade,
 usernameValutato varchar(30) references Utente (username) on delete  no action on update cascade,
-idEv integer(8) references Evento (id) on delete  no action on update cascade,
-voto integer(2) check (voto  between 0 and 10),
-primary key(usernameValutatore, usernameValutato, username)
+idEv numeric(8) references Evento (id) on delete  no action on update cascade,
+voto numeric(2) check (voto  between 0 and 10),
+check(usernameValutatore != usernameValutato),
+primary key(usernameValutatore, usernameValutato,idEv)
 );
