137589174fd2989f74b77bd7d2f1c7f392dff838
Savona Alessandro <36107882+PageFaultHandler@users.noreply.github.com>
2018-07-04 13:20:51 +0200

    Updated user insertions
    remember to modify other insertions

--- inserimento.sql
+++ inserimento.sql
@@ -1,3 +1,20 @@
+/* USER INSERTIONS */
+INSERT INTO Utente VALUES ('YouKnowNothing', 'h7eRl97T', 'Jon', 'Snow', '02/10/1997', 'Winterfell', false , '3986665289', 'S4557890', 'Filosofia','premium' );
+INSERT INTO Utente VALUES ('LupoSolitario', 'h9fQl966', 'Arya', 'Stark', '25/01/1995', 'Winterfell', true , '3996876528', 'S4265782', 'Chimica','standard' );
+INSERT INTO Utente VALUES ('MortoCheCammina', 'd5eRo93W', 'Glenn', 'Rhee', '21/12/1983', 'Yeun Sang-yeop', false , '3419535289', 'S4297611', 'Informatica', 'premium');
+INSERT INTO Utente VALUES ('LaMiaLucille', 'h7eRl97T', 'Negan', 'Morgan', '22/04/1966', 'Seattle', false , '39866793347', 'S8364027', 'Informatica' , 'premium');
+INSERT INTO Utente VALUES ('BlueMeth22', 's3jPl912', 'Jesse', 'Pinkman', '08/03/1998', 'Albuquerque', true , '3996876528', 'S4265782', 'Chimica', 'standard');
+INSERT INTO Utente VALUES ('UsatoChePiace', 'k1jGlkd6', 'Piper', 'Chapman', '06/11/1980', 'Boston', true , '3996000221', 'S4261295', 'Economia' , 'premium');
+INSERT INTO Utente VALUES ('CantaStorie', 'l0jHlhd23', 'Suzanne', 'Warren', '10/09/1992', 'Boston', true , '346898181', 'S4325914', 'Economia', 'premium' );
+INSERT INTO Utente VALUES ('StarLord', 'GhOOgj830', 'Peter Jason', 'Quill', '30/01/1965', 'Denver', true , '399765432', 'S8907319', 'Filosofia' , 'standard');
+INSERT INTO Utente VALUES ('DontKnowWhereSoulGemIs', 'Khdo97jd54', 'Gamora', 'Ben Titan', '23/09/1999', 'Planet Zen-Whoberi', true , '3996663331', 'S8395512', 'Informatica', 'standard' );
+INSERT INTO Utente VALUES ('Pagina394', 'TiOdioPotter', 'Severus', 'Piton', '01/09/1960', 'Eileen Prince', false , '3771234568', 'S4356013', 'Stregoneria', 'premium' );
+INSERT INTO Utente VALUES ('Polisucco61', 'Mad-eye11', 'Alastor', 'Moody', '21/09/1961', 'Somewhere in Scotland', false , '3359916523', 'S5491132', 'Stregoneria', 'standard' );
+INSERT INTO Utente VALUES ('HobbitCoraggioso', '748hdi8HF89', 'Bilbo', 'Baggins', '22/05/1930', 'Hobbiton', false , '339291755', 'S8229951', 'Stregoneria', 'standard' );
+
+
+/* to be modified */
+/*
 INSERT INTO Forma  VALUES (12, 132);
 INSERT INTO Forma  VALUES (13, 132);
 INSERT INTO Forma  VALUES (12, 133);
@@ -29,3 +46,4 @@ INSERT INTO Valutazione VALUES ('pippo', 'ciro', 132, 7);
 INSERT INTO Valutazione VALUES ('pluto', 'pippo', 122, 9);
 INSERT INTO Valutazione VALUES ('ciro', 'pluto', 122, 8);
 INSERT INTO Valutazione VALUES ('anna', 'pluto', 14, 1);
+*/
