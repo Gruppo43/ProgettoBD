7db23e8653119b5d1a61ae2033f543d525fecd42
Lumarti <martina.lualdi@gmail.com>
2018-07-05 18:36:40 +0200

    Update Triggers.sql

--- Triggers.sql
+++ Triggers.sql
@@ -49,11 +49,56 @@ END;
 $check_event_type_for_player$
 LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION check_state_event() RETURNS trigger AS
+$check_state_event$
+BEGIN
+	IF NEW.idEv IN (SELECT id as idEv FROM Evento WHERE stato = 'chiuso')
+		THEN return NEW;
+	ELSE RAISE EXCEPTION 'questo evento non è terminato, non e'' possibile inserire la valutazione';
+	END IF;
+END;
+$check_state_event$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION check_rating_for_player() RETURNS trigger AS
+$check_rating_for_player$
+BEGIN
+	IF NEW.usernameValutatore IN (SELECT username as usernameValutatore FROM UtenteSingoloGioca 
+					WHERE idEv IN (SELECT idEv FROM UtenteSingoloGioca WHERE username = NEW.usernameValutato))
+		THEN return NEW;
+	ELSE RAISE EXCEPTION 'i dati di giocatore valutato o di chi valuta non sono corretti';
+	END IF;
+END;
+$check_rating_for_player$
+LANGUAGE plpgsql;
+
 
 /* Triggers */
 
 CREATE TRIGGER check_premium_for_teams
-BEFORE INSERT OR UPDATE ON Squadra
+BEFORE INSERT OR UPDATE OCREATE OR REPLACE FUNCTION check_state_event() RETURNS trigger AS
+$check_state_event$
+BEGIN
+	IF NEW.idEv IN (SELECT id as idEv FROM Evento WHERE stato = 'chiuso')
+		THEN return NEW;
+	ELSE RAISE EXCEPTION 'questo evento non è terminato, non e'' possibile inserire la valutazione';
+	END IF;
+END;
+$check_state_event$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION check_rating_for_player() RETURNS trigger AS
+$check_rating_for_player$
+BEGIN
+	IF NEW.usernameValutatore IN (SELECT username as usernameValutatore FROM UtenteSingoloGioca 
+					WHERE idEv IN (SELECT idEv FROM UtenteSingoloGioca WHERE username = NEW.usernameValutato))
+		THEN return NEW;
+	ELSE RAISE EXCEPTION 'i dati di giocatore valutato o di chi valuta non sono corretti';
+	END IF;
+END;
+$check_rating_for_player$
+LANGUAGE plpgsql;
+N Squadra
 FOR EACH ROW
 EXECUTE PROCEDURE check_premium_for_teams();
 
@@ -72,4 +117,12 @@ BEFORE INSERT OR UPDATE ON UtenteSingoloGioca
 FOR EACH ROW
 EXECUTE PROCEDURE check_event_type_for_player();
 
+CREATE TRIGGER check_state_event
+BEFORE INSERT OR UPDATE ON ValutazioneUtenti
+FOR EACH ROW
+EXECUTE PROCEDURE check_state_event();
 
+CREATE TRIGGER check_rating_for_player
+BEFORE INSERT OR UPDATE ON ValutazioneUtenti
+FOR EACH ROW
+EXECUTE PROCEDURE check_rating_for_player();
