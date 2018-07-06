187303c5432de163aa4b4e9b4bf9c953fca1646a
Savona Alessandro <36107882+PageFaultHandler@users.noreply.github.com>
2018-07-06 15:01:38 +0200

    Added some user insertions

--- inserimento.sql
+++ inserimento.sql
@@ -1,17 +1,66 @@
 /* USER INSERTIONS */
+
+/* GameOfTHrones */
+
 INSERT INTO Utente VALUES ('YouKnowNothing', 'h7eRl97T', 'Jon', 'Snow', '02/10/1997', 'Winterfell', false , '3986665289', 'S4557890', 'Filosofia','premium' );
 INSERT INTO Utente VALUES ('LupoSolitario', 'h9fQl966', 'Arya', 'Stark', '25/01/1995', 'Winterfell', true , '3996876528', 'S4265782', 'Chimica','standard' );
+INSERT INTO Utente VALUES ('PortamiDelVino', 'AlloraVaERiempilo', 'Robert', 'Baratheon', '10/01/1969', 'Storm''s End', true , '3441076528', 'S4290882', 'Stregoneria','standard' );
+INSERT INTO Utente VALUES ('Inginocchiati', 'JJJo98hh7G', 'Daenerys', 'Targaryen', '29/06/2000', 'Dragon''s Stone', false , '3116806828', 'S4493782', 'Informatica','premium' );
+INSERT INTO Utente VALUES ('IoSonoReek', 'h00GGl966', 'Theon', 'Greyjoy', '13/12/1991', 'Iron Islands', true , '0805458731', 'S9944367', 'Filosofia','standard' );
+INSERT INTO Utente VALUES ('Hodor', 'HoldTheDoor', 'Wylis', 'Hodor', '18/10/1985', 'Winterfell', false , '8980458731', 'S0009071', 'Filosofia','standard' );
+
+
+/* TheWalkingDead */
+
 INSERT INTO Utente VALUES ('MortoCheCammina', 'd5eRo93W', 'Glenn', 'Rhee', '21/12/1983', 'Yeun Sang-yeop', false , '3419535289', 'S4297611', 'Informatica', 'premium');
 INSERT INTO Utente VALUES ('LaMiaLucille', 'h7eRl97T', 'Negan', 'Morgan', '22/04/1966', 'Seattle', false , '398667933', 'S8364027', 'Informatica' , 'premium');
+INSERT INTO Utente VALUES ('RealLeader', 'Glenn<3', 'Maggie', 'Greene', '16/08/1990', 'Atlanta', true , '119891245', 'S8360007', 'Informatica' , 'standard');
+INSERT INTO Utente VALUES ('MercyPrevailsWrath', 'Kd6vCRl97B', 'Rick', 'Grimes', '17/02/1971', 'Atlanta', false , '002324354', 'S8361717', 'Chimica' , 'premium');
+INSERT INTO Utente VALUES ('InfiniteArrows', 'h7eRl97T', 'Daryl', 'Dixon', '15/02/1997', 'Atlanta', false , '398668881', 'S6661227', 'Chimica' , 'standard');
+INSERT INTO Utente VALUES ('BulletMaker', 'coraggio131', 'Eugene', 'Porter', '22/04/1966', 'Atlanta', false , '8471214641', 'S8096017', 'Stregoneria' , 'standard');
+
+/* BreakingBad */
+
 INSERT INTO Utente VALUES ('BlueMeth22', 's3jPl912', 'Jesse', 'Pinkman', '08/03/1998', 'Albuquerque', true , '3996876528', 'S4265782', 'Chimica', 'standard');
+
+/* OrangeIsTheNewBlack */
+
 INSERT INTO Utente VALUES ('UsatoChePiace', 'k1jGlkd6', 'Piper', 'Chapman', '06/11/1980', 'Boston', true , '3996000221', 'S4261295', 'Economia' , 'standard');
-INSERT INTO Utente VALUES ('CantaStorie', 'l0jHlhd23', 'Suzanne', 'Warren', '10/09/1992', 'Boston', true , '346898181', 'S4325914', 'Economia', 'premium' );
+INSERT INTO Utente VALUES ('CantaStorie', 'l0jHlhd23', 'Suzanne', 'Warren', '10/09/1992', 'Boston', true , '3468981810', 'S4325914', 'Economia', 'premium' );
+
+
+/* GuardiansOfTheGalaxy */
+
+INSERT INTO Utente VALUES ('IAmGroot', 'I Am Groot', 'I Am', 'Groot', '06/11/1988', 'I Am Groot', true , '399765553', 'S89071134', 'IAmGroot' , 'standard');
 INSERT INTO Utente VALUES ('StarLord', 'GhOOgj830', 'Peter Jason', 'Quill', '30/01/1965', 'Denver', true , '399765432', 'S8907319', 'Filosofia' , 'premium');
 INSERT INTO Utente VALUES ('DontKnowWhereSoulGemIs', 'Khdo97jd54', 'Gamora', 'Ben Titan', '23/09/1999', 'Planet Zen-Whoberi', true , '3996663331', 'S8395512', 'Informatica', 'standard' );
+INSERT INTO Utente VALUES ('Boomer', 'htT67yygj990', 'Rocket', 'Raccoon', '06/11/1997', 'Somewhere in the space', true , '909765332', 'S0277319', 'Informatica' , 'premium');
+INSERT INTO Utente VALUES ('DraxTheDestroyer', '1111j830', 'Arthur Sampson', 'Douglas', '30/10/1975', 'Planet889', false , '099111131', 'S8906543', 'Filosofia' , 'standard');
+INSERT INTO Utente VALUES ('Nebula', 'gygyg6ygj990', 'Karen', 'Gillan', '11/03/1987', 'Somewhere in the space', true , '917775332', 'S9077318', 'Stregoneria' , 'standard');
+INSERT INTO Utente VALUES ('LordThanos', 'GG6J9Khgj990', 'Thanos', 'Gillan', '27/07/1995', 'Titan', true , '919075330', 'S90776668', 'Chimica' , 'premium');
+
+
+/* HarryPotter */
+
 INSERT INTO Utente VALUES ('Pagina394', 'TiOdioPotter', 'Severus', 'Piton', '01/09/1960', 'Eileen Prince', false , '3771234568', 'S4356013', 'Stregoneria', 'premium' );
 INSERT INTO Utente VALUES ('Polisucco61', 'Mad-eye11', 'Alastor', 'Moody', '21/09/1961', 'Somewhere in Scotland', false , '3359916523', 'S5491132', 'Stregoneria', 'standard' );
+INSERT INTO Utente VALUES ('OhSieteNeiGuaiStaVolta', 'AlleScalePiaceCambiare', 'Argus', 'Gazza', '23/12/1954', 'Diagon Alley', true , '3771000561', 'S8885013', 'Economia', 'standard' );
+INSERT INTO Utente VALUES ('MiDispiaceNeville', 'HHdji84Jk', 'Luna', 'Lovegood', '23/07/1999', 'Devon', false , '3159910005', 'S5001132', 'Informatica', 'standard' );
+INSERT INTO Utente VALUES ('LordVoldemort', 'AvadaKedavra', 'Tom Marvolo', 'Riddle', '26/04/1971', 'London', false , '8879916123', 'S5091092', 'Filosofia', 'premium' );
+INSERT INTO Utente VALUES ('PerchèSeguiteIRagni?', 'paperella14', 'Ronald', 'Weasley', '03/05/1998', 'London', true , '8879522123', 'S8884230', 'Filosofia', 'standard' );
+INSERT INTO Utente VALUES ('ElfoLibero', 'Calzino', 'Dobby', 'NoSurname', '09/06/2001', 'London', true , '8811266123', 'S88001120', 'Chimica', 'standard' );
+
+--OhSieteNeiGuaiStaVolta
+/* LordOfRings */
+
 INSERT INTO Utente VALUES ('HobbitCoraggioso', '748hdi8HF89', 'Bilbo', 'Baggins', '22/05/1930', 'Hobbiton', false , '339291755', 'S8229951', 'Stregoneria', 'premium' );
 
+/* DarkSoul */
+
+INSERT INTO Utente VALUES ('SoEaslyForgotten', '7379dhu38', 'Furtive', 'Pygmy ', '19/02/1978', 'The Abyss', true , '3155316573', 'S5191137', 'Chimica', 'standard' );
+INSERT INTO Utente VALUES ('PraiseTheSun', '737jfin438', 'Solaire', 'Of Astora', '15/04/1982', 'Astora', true , '3177416571', 'S6691130', 'Filosofia', 'premium' );
+INSERT INTO Utente VALUES ('BraveWarrior', '737fnj49jJ', 'Siegmeyer', 'Of Catarina', '15/04/1982', 'Catarina', false , '3117999467', 'S6953138', 'Economia', 'premium' );
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 /* Categories */
 
