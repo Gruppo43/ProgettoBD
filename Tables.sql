dce3a9255817249fdbe28ce77919dbb298e8708b
Savona Alessandro <36107882+PageFaultHandler@users.noreply.github.com>
2018-07-06 11:25:48 +0200

    fixed some conflicts, added some check

--- Tables.sql
+++ Tables.sql
@@ -2,25 +2,25 @@ set search_path to 'ProgettoBD';
 
 
 CREATE TABLE Utente(
-username varchar(22) primary key,
+username varchar(25) primary key,
 psw varchar(20) not null,
 nome varchar(20) not null,
 cognome varchar(20) not null,
 annoN date not null,
 luogoN varchar(35) not null,
-foto boolean,
+foto boolean not null,
 tel varchar(10) not null,
 matricola varchar(8) not null,
 CorsoDiStudio varchar(20) not null,
 tipo varchar(8) not null
 check (tipo = 'standard' or tipo = 'premium')
 check (annoN < current_date),
-check(tel SIMILAR TO '([0-9]+)')
+check(tel similar to '([0-9]+)')
 );
 
 CREATE TABLE Categoria(
 nome varchar(20) primary key,
-regolamento varchar(50) not null,
+regolamento varchar(500) not null,
 numGiocatori numeric(2) not null,
 foto boolean default false
 );
@@ -28,8 +28,8 @@ foto boolean default false
 CREATE TABLE Torneo(
 id numeric(8) primary key,
 restrizioni varchar(50) default null,
-descrizione varchar(60) default null,
-modalità varchar(35) not null,
+descrizione varchar(100) default null,
+modalità varchar(50) not null,
 categoria varchar(20) not null,
 sponsor varchar(20) default null,
 premi varchar(20) not null,
@@ -38,7 +38,7 @@ creatore varchar(20) default null,
 foreign key(creatore) references Utente(username) on delete  no action on update cascade,
 foreign key(categoria) references Categoria(nome) on delete no action on update cascade,
 check(tipo = 'singolo' or tipo = 'a squadre'),
-check(modalità = 'Eliminazione diretta' or modalità = 'Girone all''italiana' or modalità = 'Misto')
+check(modalità = 'eliminazione diretta' or modalità = 'girone all''italiana' or modalità = 'misto')
 );
 
 
@@ -47,7 +47,7 @@ check(modalità = 'Eliminazione diretta' or modalità = 'Girone all''italiana' o
 CREATE TABLE Squadra(
 nome varchar(30) not null,
 categoria varchar(20) not null,
-creatore varchar(20) default null,
+creatore varchar(25) default null,
 maxGiocatori numeric(2) not null,
 minGiocatori numeric(2) not null,
 coloreMaglia varchar(10) not null,
@@ -62,13 +62,13 @@ check(minGiocatori > 0)
 
 
 CREATE TABLE Impianto (
-nome varchar(20) primary key,
+nome varchar(25) primary key,
 via varchar(50) not null,
 telefono varchar(10) not null,
 email varchar(20) not null,
 longitudine float not null,
 latitudine float not null,
-check(telefono SIMILAR TO '([0-9]+)')
+check(telefono similar to '([0-9]+)')
 );
 
 
@@ -77,9 +77,9 @@ id numeric(8) primary key,
 data date not null,
 stato varchar(15) not null,
 durata numeric(3) not null,
-impianto varchar(20) not null,
+impianto varchar(25) not null,
 categoria varchar(20) not null,
-tipo varchar(15) not null,
+tipo varchar(10) not null,
 foreign key(impianto) references Impianto(nome) on delete  no action on update cascade,
 foreign key(categoria) references Categoria(nome) on delete  no action on update cascade,
 check(data <= current_date),
@@ -102,18 +102,20 @@ nomeC varchar(30),
 idEv numeric(8) references Evento (id) on delete  no action on update cascade,
 punti numeric(5) not null,
 foreign key(nomeSquadra,nomeC) references Squadra(nome,categoria),
-primary key(nomeSquadra, nomeC, idEv)
+primary key(nomeSquadra, nomeC, idEv),
+check(punti >= 0)
 );
 
 CREATE TABLE UtenteSingoloGioca(
-username varchar(30) references Utente (username) on delete  no action on update cascade,
+username varchar(25) references Utente (username) on delete  no action on update cascade,
 idEv numeric(8) references Evento (id) on delete  no action on update cascade,
 setvinti numeric(3) not null,
-primary key(username, idEv)
+primary key(username, idEv),
+check(setvinti >= 0)
 );
 
 CREATE TABLE MatchDisputati(
-username varchar(30) references Utente (username)on delete  no action on update cascade,
+username varchar(25) references Utente (username)on delete  no action on update cascade,
 nomeC varchar(30) references Categoria (nome) on delete  no action on update cascade,
 gareDisputate numeric(5),
 primary key(username, nomeC)
@@ -129,63 +131,64 @@ primary key(usernameValutatore, usernameValutato,idEv)
 );
 
 CREATE TABLE Forum(
-CorsoDiStudi VARCHAR(50),
-Categoria VARCHAR(20),
-FOREIGN KEY (Categoria)references Categoria(Nome) on delete  no action on update cascade,
-PRIMARY KEY(CorsoDiStudi,Categoria)
+corsoDiStudi varchar(50),
+categoria varchar(20),
+foreign key (categoria)references Categoria(nome) on delete  no action on update cascade,
+primary key(corsoDiStudi,categoria)
 );
 
 CREATE TABLE Post(
-Data CHAR(9),
-Testo VARCHAR(800) NOT NULL,
-Foto BOOLEAN DEFAULT NULL,
-Creatore VARCHAR(22) NOT NULL,
-CorsoDiStudi VARCHAR(20) NOT NULL,  
-Categoria VARCHAR(20) NOT NULL, 
-FOREIGN KEY (Creatore)references Utente(Username) on delete  no action on update cascade,
-FOREIGN KEY (CorsoDiStudi,Categoria) references Forum(CorsoDiStudi,Categoria) on delete  no action on update cascade,
-PRIMARY KEY(Data,Creatore)
+data date,
+testo varchar(800) not null,
+foto boolean not null,
+creatore varchar(22) not null,
+corsoDiStudi varchar(20) not null,  
+categoria varchar(20) not null, 
+foreign key (creatore)references Utente(username) on delete  no action on update cascade,
+foreign key (corsoDiStudi,categoria) references Forum(corsoDiStudi,categoria) on delete  no action on update cascade,
+primary key(data,creatore),
+check(data <= current_date)
 );
 
 CREATE TABLE Candidatura(
-Stato VARCHAR(9) NOT NULL,
-Supervisore VARCHAR(22) NOT NULL,
-Candidato VARCHAR(22) NOT NULL,
-Squadra VARCHAR(30) NOT NULL,
-Categoria VARCHAR(20) NOT NULL,
-FOREIGN KEY (Candidato)references Utente(Username) on delete  no action on update cascade,
-FOREIGN KEY (Squadra,Categoria)references Squadra(Nome,Categoria) on delete  no action on update cascade,
-FOREIGN KEY (Supervisore)references Utente(Username),
-PRIMARY KEY (Candidato,Squadra),
-CHECK (Stato = 'Accettata' OR Stato = 'Rifiutata'),
-CHECK (Supervisore != Candidato)
+stato varchar(9) not null,
+supervisore varchar(25) not null,
+candidato varchar(25) not null,
+squadra varchar(30) not null,
+categoria varchar(20) not null,
+foreign key (candidato)references Utente(username) on delete  no action on update cascade,
+foreign key (squadra,categoria)references Squadra(nome,categoria) on delete  no action on update cascade,
+foreign key (supervisore)references Utente(username),
+primary key (Candidato,Squadra),
+check (stato = 'accettata' or stato = 'rifiutata'),
+check (supervisore != candidato)
 );
 
 CREATE TABLE Iscrizione(
-Data CHAR(9),
-Stato VARCHAR(9) NOT NULL,
-Studente VARCHAR(22) NOT NULL,
-Evento NUMERIC(8) NOT NULL,
-FOREIGN KEY (Studente)references Utente(Username) on delete  no action on update cascade,
-FOREIGN KEY (Evento)references Evento(Id) on delete  no action on update cascade,
-PRIMARY KEY (Data,Studente,Evento),
-CHECK (Stato = 'Confermato' OR Stato = 'Rifiutato')
+data date,
+stato varchar(9) not null,
+studente varchar(25) not null,
+evento numeric(8) not null,
+foreign key (studente)references Utente(username) on delete  no action on update cascade,
+foreign key (evento)references Evento(id) on delete  no action on update cascade,
+primary key (data,studente,evento),
+check (stato = 'confermato' or stato = 'rifiutato'),
+check(data <= current_date)
 );
 
 CREATE TABLE PartecipaATorneo(
-Torneo NUMERIC(8) NOT NULL,
-Squadra VARCHAR(30) NOT NULL, 
-Categoria VARCHAR(20) NOT NULL,
-FOREIGN KEY (Torneo)references Torneo(Id)on delete  no action on update cascade,
-FOREIGN KEY (Squadra,Categoria)references Squadra(Nome,Categoria) on delete  no action on update cascade, 
-FOREIGN KEY (Categoria)references Categoria(Nome) on delete  no action on update cascade,
-PRIMARY KEY (Torneo,Squadra,Categoria)
+torneo numeric(8) not null,
+squadra varchar(30) not null, 
+categoria varchar(20) not null,
+foreign key (torneo)references Torneo(id)on delete  no action on update cascade,
+foreign key (squadra,categoria)references Squadra(nome,categoria) on delete  no action on update cascade, 
+primary key (torneo,squadra,categoria)
 );
 
 CREATE TABLE IscrittoATorneo(
-Torneo NUMERIC(8),
-Studente VARCHAR(22),
-FOREIGN KEY (Torneo)references Torneo(Id) on delete  no action on update cascade,
-FOREIGN KEY(Studente)references Utente(Username) on delete  no action on update cascade,
-PRIMARY KEY (Torneo,Studente)
+torneo numeric(8),
+studente varchar(25),
+foreign key (torneo)references Torneo(id) on delete  no action on update cascade,
+foreign key(studente)references Utente(username) on delete  no action on update cascade,
+primary key (torneo,studente)
 );
