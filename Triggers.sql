91705969ecb58c7aae9368359207d934d8f33af2
Savona Alessandro <36107882+PageFaultHandler@users.noreply.github.com>
2018-07-19 16:04:46 +0200

    Deleted some triggers, updated others..

--- Triggers.sql
+++ Triggers.sql
@@ -1,7 +1,7 @@
 
 
-/* Functions */
 
+--solo un utente premium può creare una squadra
 CREATE OR REPLACE FUNCTION check_premium_for_teams() RETURNS trigger AS
 $check_premium_for_teams$
 BEGIN
@@ -14,6 +14,7 @@ $check_premium_for_teams$
 LANGUAGE plpgsql;
 
 
+--il numero minimo massimo di giocatori di una squadra non può essere minore del numero minimo previsto per quella categoria
 CREATE OR REPLACE FUNCTION check_players_number() RETURNS trigger AS
 $check_players_number$
 BEGIN
@@ -25,32 +26,7 @@ END;
 $check_players_number$
 LANGUAGE plpgsql;
 
-
-CREATE OR REPLACE FUNCTION check_event_type_for_team() RETURNS trigger AS
-$check_event_type_for_team$
-BEGIN
-	IF NEW.idEv NOT IN (SELECT id  FROM Evento WHERE tipo = 'singolo')
-		THEN RETURN NEW;
-	ELSE  RAISE EXCEPTION 'una squadra non può partecipare ad un evento per singoli';
-	END IF;
-END;
-$check_event_type_for_team$
-LANGUAGE plpgsql;
-
-
-
-CREATE OR REPLACE FUNCTION check_event_type_for_player() RETURNS trigger AS
-$check_event_type_for_player$
-BEGIN
-	IF NEW.idEv NOT IN (SELECT id  FROM Evento WHERE tipo = 'a squadre')
-		THEN RETURN NEW;
-	ELSE  RAISE EXCEPTION 'un singolo non può giocare in un evento a squadre!';
-	END IF;
-END;
-$check_event_type_for_player$
-LANGUAGE plpgsql;
-
-
+--ci si può valutare solamente se l'evento si è chiuso
 CREATE OR REPLACE FUNCTION check_state_event() RETURNS trigger AS
 $check_state_event$
 BEGIN
@@ -63,6 +39,7 @@ $check_state_event$
 LANGUAGE plpgsql;
 
 
+-- verificare che il valutatore ed il valutato abbiano giocato effettivamente lo stesso evento
 CREATE OR REPLACE FUNCTION check_rating_for_player() RETURNS trigger AS
 $check_rating_for_player$
 BEGIN
@@ -77,7 +54,6 @@ LANGUAGE plpgsql;
 
 
 -- un impianto non puo ospitare due eventi contemporaneamente --
-
 CREATE OR REPLACE FUNCTION check_stadium_for_evento() RETURNS trigger AS
 $check_stadium_for_evento$
 BEGIN
@@ -89,6 +65,109 @@ END;
 $check_stadium_for_evento$
 LANGUAGE plpgsql;
 
+
+-- un evento che deve ancora svolgersi non puo' avere lo stato chiuso. Un evento aperto non puo' essere già finito!
+CREATE OR REPLACE FUNCTION check_event_date() RETURNS trigger AS
+$check_event_date$
+BEGIN
+	IF (NEW.data <= current_date)
+		THEN IF (NEW.stato = 'chiuso')
+				THEN RETURN NEW;
+			ELSE RAISE EXCEPTION 'l''evento % è stato svolto in data % , ergo non può avere lo stato ''aperto''',NEW.id,NEW.data;
+			END IF;
+	ELSE 
+		IF(NEW.stato = 'aperto')
+			THEN RETURN NEW;
+		ELSE RAISE EXCEPTION 'l''evento % si svolgerà in data % , ergo non può avere lo stato ''chiuso''',NEW.id,NEW.data;
+		END IF;
+	END IF;
+END;
+$check_event_date$
+LANGUAGE plpgsql;
+
+
+-- un evento deve essere della stessa categoria del torneo di cui fa parte!
+CREATE OR REPLACE FUNCTION check_tournament_category() RETURNS trigger AS
+$check_tournament_category$
+BEGIN
+	IF ((SELECT categoria FROM Torneo where id = NEW.idT) <> (SELECT categoria FROM Evento WHERE id = NEW.idEV))
+		THEN RAISE EXCEPTION 'L''evento % fa parte di una categoria divergente da quella del torneo %',NEW.idEV,NEW.idT;
+	ELSE RETURN NEW;
+	END IF;
+END;
+$check_tournament_category$
+LANGUAGE plpgsql;
+
+
+
+-- un determinato evento non puo far parte di due o piu tornei
+CREATE OR REPLACE FUNCTION check_unique_event_in_tournament() RETURNS trigger AS
+$check_unique_event_in_tournament$
+BEGIN
+	IF (SELECT count(*) FROM EventoInTorneo WHERE idEV = NEW.idEV) = 1
+		THEN RAISE EXCEPTION 'L''evento % fa già parte di un altro torneo!',NEW.idEV;
+	ELSE RETURN NEW;
+	END IF;
+END;
+$check_unique_event_in_tournament$
+LANGUAGE plpgsql;
+
+
+-- solo un utente premium puo creare un torneo
+CREATE OR REPLACE FUNCTION check_premium_for_tournament() RETURNS trigger AS
+$check_premium_for_tournament$
+BEGIN
+	IF NEW.creatore NOT IN (SELECT username  FROM Utente WHERE tipo = 'standard')
+		THEN RETURN NEW;
+	ELSE  RAISE EXCEPTION 'L''utente % non è premium, non può creare il torneo!',NEW.creatore;
+	END IF;
+END;
+$check_premium_for_tournament$
+LANGUAGE plpgsql;
+
+--solo un utente premium può accettare o rifiutare le candidature
+CREATE OR REPLACE FUNCTION check_premium_accepts_applications() RETURNS trigger AS 
+$check_premium_accepts_applications$
+BEGIN
+	IF 
+		NEW.Supervisore NOT IN (SELECT Username  FROM Utente WHERE Tipo = 'standard')
+	THEN
+		RETURN NEW;
+	ELSE
+		RAISE EXCEPTION 'Solo un utente premium può accettare o rifiutare le candidature!';
+	END IF;
+END;
+$check_premium_accepts_applications$
+LANGUAGE plpgsql;
+			
+
+--un utente può iscriversi solo ad un evento aperto
+CREATE OR REPLACE FUNCTION sign_up_for_open_events_only() RETURNS trigger AS
+$sign_up_for_open_events_only$
+BEGIN
+	IF NEW.Evento NOT IN (SELECT Id  FROM Evento WHERE Stato = 'chiuso')
+	THEN RETURN NEW;
+	ELSE RAISE EXCEPTION 'Un utente può iscriversi solo ad un evento aperto!';
+	END IF;
+END;
+$sign_up_for_open_events_only$
+LANGUAGE plpgsql;
+
+
+-- non si puo aggiungere un esito fi un evento aperto
+CREATE OR REPLACE FUNCTION check_if_accessible_event() RETURNS trigger AS
+$check_if_accessible_event$
+BEGIN
+	IF ((SELECT stato FROM Evento WHERE id = NEW.idEV) = 'aperto')
+		THEN RAISE EXCEPTION 'L''evento % risulta essere aperto, non è possibile quindi aggiuungere un esito!
+		Se l''evento si è concluso preghiamo il gestore del database di modificare lo stato dell''evento da ''aperto''
+		a ''chiuso''',NEW.idEv;
+	
+	ELSE RETURN NEW;
+	END IF;
+END;
+$check_if_accessible_event$
+LANGUAGE plpgsql;
 
 
 -- una squadra non può giocare due eventi contemporaneamente per prima squadra --
@@ -158,199 +237,25 @@ $check_player2_for_matches$
 LANGUAGE plpgsql;
 
 
-
-
--- ad un evento singolo possono giocare solo due giocatori --
-
-CREATE OR REPLACE FUNCTION check_partecipants_number_for_player() RETURNS trigger AS
-$check_partecipants_number_for_player$
-BEGIN
-	IF (SELECT COUNT(*) FROM UtenteSingoloGioca WHERE idEv = NEW.idEv) <= 1
-		THEN RETURN NEW;
-	ELSE  RAISE EXCEPTION 'A questo evento: % possono partecipare solo due giocatori!',NEW.idEV;
-	END IF;
-END;
-$check_partecipants_number_for_player$
-LANGUAGE plpgsql;
-
-
--- ad un evento a squadre possono giocare solo due squadre --
-
-CREATE OR REPLACE FUNCTION check_partecipants_number_for_team() RETURNS trigger AS
-$check_partecipants_number_for_team$
-BEGIN
-	IF (SELECT COUNT(*) FROM SquadraPartecipaEv WHERE idEv = NEW.idEv) <= 1
-		THEN RETURN NEW;
-	ELSE  RAISE EXCEPTION 'A questo evento: % possono partecipare solo due squadre!',NEW.idEV;
-	END IF;
-END;
-$check_partecipants_number_for_team$
-LANGUAGE plpgsql;
-
-
-
--- un evento che deve ancora svolgersi non puo' avere lo stato chiuso. Un evento aperto non puo' essere già finito!
-CREATE OR REPLACE FUNCTION check_event_date() RETURNS trigger AS
-$check_event_date$
-BEGIN
-	IF (NEW.data <= current_date)
-		THEN IF (NEW.stato = 'chiuso')
-				THEN RETURN NEW;
-			ELSE RAISE EXCEPTION 'l''evento % è stato svolto in data % , ergo non può avere lo stato ''aperto''',NEW.id,NEW.data;
-			END IF;
-	ELSE 
-		IF(NEW.stato = 'aperto')
-			THEN RETURN NEW;
-		ELSE RAISE EXCEPTION 'l''evento % si svolgerà in data % , ergo non può avere lo stato ''chiuso''',NEW.id,NEW.data;
-		END IF;
-	END IF;
-END;
-$check_event_date$
-LANGUAGE plpgsql;
-
-
--- un evento deve essere della stessa categoria del torneo di cui fa parte!
-CREATE OR REPLACE FUNCTION check_tournament_category() RETURNS trigger AS
-$check_tournament_category$
-BEGIN
-	IF ((SELECT categoria FROM Torneo where id = NEW.idT) <> (SELECT categoria FROM Evento WHERE id = NEW.idEV))
-		THEN RAISE EXCEPTION 'L''evento % fa parte di una categoria divergente da quella del torneo %',NEW.idEV,NEW.idT;
-	ELSE RETURN NEW;
-	END IF;
-END;
-$check_tournament_category$
-LANGUAGE plpgsql;
-
-
--- un determinato evento non puo far parte di due o piu tornei
-CREATE OR REPLACE FUNCTION check_unique_event_in_tournament() RETURNS trigger AS
-$check_unique_event_in_tournament$
-BEGIN
-	IF (SELECT count(*) FROM EventoInTorneo WHERE idEV = NEW.idEV) = 1
-		THEN RAISE EXCEPTION 'L''evento % fa già parte di un altro torneo!',NEW.idEV;
-	ELSE RETURN NEW;
-	END IF;
-END;
-$check_unique_event_in_tournament$
-LANGUAGE plpgsql;
-
-
--- solo un utente premium puo creare un torneo
-CREATE OR REPLACE FUNCTION check_premium_for_tournament() RETURNS trigger AS
-$check_premium_for_tournament$
-BEGIN
-	IF NEW.creatore NOT IN (SELECT username  FROM Utente WHERE tipo = 'standard')
-		THEN RETURN NEW;
-	ELSE  RAISE EXCEPTION 'L''utente % non è premium, non può creare il torneo!',NEW.creatore;
-	END IF;
-END;
-$check_premium_for_tournament$
-LANGUAGE plpgsql;
-
-
--- non si puo aggiungere un esito fi un evento aperto
-CREATE OR REPLACE FUNCTION check_if_accessible_event() RETURNS trigger AS
-$check_if_accessible_event$
-BEGIN
-	IF ((SELECT stato FROM Evento WHERE id = NEW.idEV) = 'aperto')
-		THEN RAISE EXCEPTION 'L''evento % risulta essere aperto, non è possibile quindi aggiuungere un esito!
-		Se l''evento si è concluso preghiamo il gestore del database di modificare lo stato dell''evento da ''aperto''
-		a ''chiuso''',NEW.idEv;
-	
-	ELSE RETURN NEW;
-	END IF;
-END;
-$check_if_accessible_event$
-LANGUAGE plpgsql;
-
-
---solo un utente premium può accettare o rifiutare le candidature
-CREATE OR REPLACE FUNCTION check_premium_accepts_applications() RETURNS trigger AS 
-$check_premium_accepts_applications$
-BEGIN
-	IF 
-		NEW.Supervisore NOT IN (SELECT Username  FROM Utente WHERE Tipo = 'standard')
-	THEN
-		RETURN NEW;
-	ELSE
-		RAISE EXCEPTION 'Solo un utente premium può accettare o rifiutare le candidature!';
-	END IF;
-END;
-$check_premium_accepts_applications$
-LANGUAGE plpgsql;
-				
---un utente può iscriversi solo ad un evento aperto
-CREATE OR REPLACE FUNCTION sign_up_for_open_events_only() RETURNS trigger AS
-$sign_up_for_open_events_only$
-BEGIN
-	IF NEW.Evento NOT IN (SELECT Id  FROM Evento WHERE Stato = 'chiuso')
-	THEN RETURN NEW;
-	ELSE RAISE EXCEPTION 'Un utente può iscriversi solo ad un evento aperto!';
-	END IF;
-END;
-$sign_up_for_open_events_only$
-LANGUAGE plpgsql;
-
--- una squadra non puo partecipare ad un evento se non ha raggiunto il numero minimo di candidature
-CREATE OR REPLACE FUNCTION check_min_players_for_team() RETURNS trigger AS
-$check_min_players_for_team$
-BEGIN
-	IF ((SELECT count(*) FROM Candidatura WHERE Squadra = NEW.nomeSquadra AND categoria = NEW.nomeC AND stato = 'accettata') 
-		>= (SELECT minGiocatori FROM Squadra WHERE nome = NEW.nomeSquadra AND categoria = NEW.nomeC))
-		THEN RETURN NEW;
-	ELSE  RAISE EXCEPTION 'la squadra % della categoria % non ha raggiunto il numero minimo di candidature per disputare questo
-	evento',NEW.nomeSquadra,NEW.nomeC;
-	END IF;
-END;
-$check_min_players_for_team$
-LANGUAGE plpgsql;
-
-
--- una squadra non puo' superare il numero massimo di giocatori previsto
-CREATE OR REPLACE FUNCTION check_max_players_for_team() RETURNS trigger AS
-$check_max_players_for_team$
-BEGIN
-	IF (NEW.stato = 'accettata')
-		THEN IF ((SELECT count(*) FROM Candidatura WHERE squadra = NEW.squadra AND categoria = NEW.categoria) = 
-				(SELECT maxGiocatori FROM Squadra WHERE nome = NEW.squadra AND categoria = NEW.categoria))
-		 		THEN RAISE EXCEPTION 'Non si possono accettare ulteriori candidature per l squadra %
-			 	della categoria %',NEW.Squadra,NEW.categoria;
-			 ELSE RETURN NEW;
-			END IF;
-	ELSE  RETURN NEW;
-	END IF;
-END;
-$check_max_players_for_team$
-LANGUAGE plpgsql;
-
-
-/* Triggers */
-
+--solo un utente premium può creare una squadra
 CREATE TRIGGER check_premium_for_teams
 BEFORE INSERT OR UPDATE ON Squadra
 FOR EACH ROW
 EXECUTE PROCEDURE check_premium_for_teams();
 
+--il numero minimo massimo di giocatori di una squadra non può essere minore del numero minimo previsto per quella categoria
 CREATE TRIGGER check_players_number
 BEFORE INSERT OR UPDATE ON Squadra
 FOR EACH ROW
 EXECUTE PROCEDURE check_players_number();
 
-CREATE TRIGGER check_event_type_for_team
-BEFORE INSERT OR UPDATE ON SquadraPartecipaEv
-FOR EACH ROW
-EXECUTE PROCEDURE check_event_type_for_team();
-
-CREATE TRIGGER check_event_type_for_player
-BEFORE INSERT OR UPDATE ON UtenteSingoloGioca
-FOR EACH ROW
-EXECUTE PROCEDURE check_event_type_for_player();
-
+--ci si può valutare solamente se l'evento si è chiuso
 CREATE TRIGGER check_state_event
 BEFORE INSERT OR UPDATE ON ValutazioneUtenti
 FOR EACH ROW
 EXECUTE PROCEDURE check_state_event();
 
+--due utenti devono aver partecipato al medesimo evento per potersi valutare
 CREATE TRIGGER check_rating_for_player
 BEFORE INSERT OR UPDATE ON ValutazioneUtenti
 FOR EACH ROW
@@ -361,48 +266,6 @@ CREATE TRIGGER check_stadium_for_event
 BEFORE INSERT OR UPDATE ON Evento
 FOR EACH ROW
 EXECUTE PROCEDURE check_stadium_for_evento();
-
--- una squadra non può giocare due eventi contemporaneamente per prima squadra --
-CREATE TRIGGER check_team2_for_matches
-BEFORE INSERT OR UPDATE ON EsitoSquadre
-FOR EACH ROW
-EXECUTE PROCEDURE check_team2_for_matches();
-				 
- -- una squadra non può giocare due eventi contemporaneamente per seconda squadra --
-CREATE TRIGGER check_team1_for_matches
-BEFORE INSERT OR UPDATE ON EsitoSquadre
-FOR EACH ROW
-EXECUTE PROCEDURE check_team1_for_matches();
-				 
- -- una giocatore non può giocare due eventi contemporaneamente per primo giocatore --			 
-CREATE TRIGGER check_player2_for_matches
-BEFORE INSERT OR UPDATE ON EsitoSingolo
-FOR EACH ROW
-EXECUTE PROCEDURE check_player2_for_matches();
-				 
- -- una giocatore non può giocare due eventi contemporaneamente per secondo giocatore --			 
-CREATE TRIGGER check_player1_for_matches
-BEFORE INSERT OR UPDATE ON EsitoSingolo
-FOR EACH ROW
-EXECUTE PROCEDURE check_player1_for_matches();				 
-
--- una giocatore non può giocare due eventi contemporaneamente --
-CREATE TRIGGER check_player_for_matches
-BEFORE INSERT OR UPDATE ON UtenteSingoloGioca
-FOR EACH ROW
-EXECUTE PROCEDURE check_player_for_matches();
-
--- ad un evento singolo possono giocare solo due giocatori --
-CREATE TRIGGER check_partecipants_number_for_player
-BEFORE INSERT OR UPDATE ON UtenteSingoloGioca
-FOR EACH ROW
-EXECUTE PROCEDURE check_partecipants_number_for_player();
-
--- ad un evento a squadre possono giocare solo due squadre --
-CREATE TRIGGER check_partecipants_number_for_team
-BEFORE INSERT OR UPDATE ON SquadraPartecipaEv
-FOR EACH ROW
-EXECUTE PROCEDURE check_partecipants_number_for_team();
 
 -- un evento che deve ancora svolgersi non puo' avere lo stato chiuso. Un evento aperto non puo' essere già finito!
 CREATE TRIGGER check_event_date
@@ -428,12 +291,6 @@ BEFORE INSERT OR UPDATE ON Torneo
 FOR EACH ROW
 EXECUTE PROCEDURE check_premium_for_tournament();
 
--- non si puo aggiungere un esito di un evento aperto
-CREATE TRIGGER check_if_accessible_event_for_team
-BEFORE INSERT OR UPDATE ON SquadraPartecipaEv
-FOR EACH ROW
-EXECUTE PROCEDURE check_if_accessible_event();
-
 --solo un utente premium può accettare o rifiutare le candidature
 CREATE TRIGGER check_premius_accepts_applications
 BEFORE INSERT OR UPDATE ON Candidatura
@@ -448,13 +305,36 @@ EXECUTE PROCEDURE sign_up_for_open_events_only();
 
 -- non si puo aggiungere un esito di un evento aperto
 CREATE  TRIGGER check_if_accessible_event_for_player
-BEFORE INSERT OR UPDATE ON UtenteSingoloGioca
+BEFORE INSERT OR UPDATE ON EsitoSingolo
 FOR EACH ROW
 EXECUTE PROCEDURE check_if_accessible_event();
 
--- una squadra non puo partecipare ad un evento se non ha raggiunto il numero minimo di candidature
-CREATE  TRIGGER check_min_players_for_team
-BEFORE INSERT OR UPDATE ON SquadraPartecipaEv
+-- non si puo aggiungere un esito di un evento aperto
+CREATE  TRIGGER check_if_accessible_event_for_player
+BEFORE INSERT OR UPDATE ON EsitoSquadre
 FOR EACH ROW
-EXECUTE PROCEDURE check_min_players_for_team();
+EXECUTE PROCEDURE check_if_accessible_event();
 
+-- una squadra non può giocare due eventi contemporaneamente per prima squadra --
+CREATE TRIGGER check_team2_for_matches
+BEFORE INSERT OR UPDATE ON EsitoSquadre
+FOR EACH ROW
+EXECUTE PROCEDURE check_team2_for_matches();
+				 
+ -- una squadra non può giocare due eventi contemporaneamente per seconda squadra --
+CREATE TRIGGER check_team1_for_matches
+BEFORE INSERT OR UPDATE ON EsitoSquadre
+FOR EACH ROW
+EXECUTE PROCEDURE check_team1_for_matches();
+				 
+ -- una giocatore non può giocare due eventi contemporaneamente per primo giocatore --			 
+CREATE TRIGGER check_player2_for_matches
+BEFORE INSERT OR UPDATE ON EsitoSingolo
+FOR EACH ROW
+EXECUTE PROCEDURE check_player2_for_matches();
+				 
+ -- una giocatore non può giocare due eventi contemporaneamente per secondo giocatore --			 
+CREATE TRIGGER check_player1_for_matches
+BEFORE INSERT OR UPDATE ON EsitoSingolo
+FOR EACH ROW
+EXECUTE PROCEDURE check_player1_for_matches(); 
