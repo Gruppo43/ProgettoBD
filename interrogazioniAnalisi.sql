5004e98248b91ac757b73b6142da1faa1a73cddc
Lumarti <martina.lualdi@gmail.com>
2018-07-24 04:01:06 +0200

    Update interrogazioniAnalisi.sql

--- interrogazioniAnalisi.sql
+++ interrogazioniAnalisi.sql
@@ -48,12 +48,34 @@ HAVING COUNT(id) >= ALL (SELECT COUNT(E.id) FROM Evento E  WHERE categoria = E.c
 GROUP BY E.categoria);
 
 /* c. determinare per ogni categoria e corso di studi la frazione di partecipanti a eventi di
-quella categoria con età < 20 anni 
+quella categoria con età < 20 anni
 */
 
-SELECT categoria, CorsoDiStudio, COUNT(DISTINCT username) FROM Utente 
-JOIN Iscrizione ON studente = username
+SELECT categoria, CorsoDiStudio, COUNT(studente) AS partecipanti_ventenni FROM Iscrizione
 JOIN Evento ON id = evento
-AND Iscrizione.stato = 'confermato'
+JOIN Utente U ON studente = username
 WHERE  (CURRENT_DATE - annon)/365 <= 20
-GROUP BY categoria, CorsoDiStudio;
+GROUP BY categoria, CorsoDiStudio
+HAVING COUNT(studente) >= (SELECT COUNT(username) FROM Utente 
+				WHERE  (CURRENT_DATE - annon)/365 <= 20
+				AND U.CorsoDIStudio = CorsoDiStudio
+				GROUP BY CorsoDiStudio)
+ORDER BY categoria;
+
+
+/* d. minuti totali disputati per impianto e categoria
+*/
+SELECT impianto, SUM(durata), categoria FROM Evento 
+JOIN Iscrizione I ON evento = id 
+GROUP BY categoria, impianto ORDER BY impianto;
+
+/* e. numero partecipanti per ogni corso di studi
+*/
+SELECT COUNT(studente), corsodistudio FROM Iscrizione JOIN Utente ON username = studente
+WHERE stato = 'confermato' GROUP BY corsodistudio;
+
+/* f. studenti che hanno disputato partite per categoria
+*/
+SELECT categoria, COUNT(studente) FROM Iscrizione JOIN Evento ON evento = id
+WHERE iscrizione.stato = 'confermato'
+GROUP BY categoria;
